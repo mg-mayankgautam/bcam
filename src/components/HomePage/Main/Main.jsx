1f4f931b67e6b19c6e693a300b69a10715111fec
Mansha <manshapreet02@gmail.com>
2024-10-28 21:37:06 +0530

    main pg

--- src/components/HomePage/Main/Main.jsx
+++ src/components/HomePage/Main/Main.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import './Main.css';
-import img from '../../../assets/main.png';
+import img from '../../../assets/logowhite.png';
 
 const Main = () => {
     return (
         <div className='HomeMain'>
             <div className='mainHeader'>
-                <div className='mainHeading'>
+                {/* <div className='mainHeading'>
                     <strong>
                         <span className='orange'> Be</span>
                         <span className='blue'>CAM</span>
                     </strong>
                     <br />
-                    Feel <strong>SAFE</strong> With Us
+                </div> */}
 
+                <div className='mainImg'>
+                    <img src={img} />
                 </div>
+
+
                 <div className='mainText'>
-                    <div>BE-CAM is your trusted partner in providing innovative and reliable CCTV surveillance solutions. <br/>With over 5 years of experience, we offer customized security systems for homes, businesses, and communities. 
-                    <br/>Whether you're protecting loved ones or ensuring the safety of your business, we deliver <br/>advanced, real-time monitoring solutions to meet your needs.
+
+                    <div className='mainTagline'>
+                        "Feel <strong className='orange italic'>SAFE</strong> with us"
+                    </div>
+
+                    <div>
+                        BE-CAM is your trusted partner in providing innovative and reliable CCTV surveillance solutions. With over 5 years of experience, we offer customized security systems for homes, businesses, and communities.
+                        Whether you're protecting loved ones or ensuring the safety of your business, we deliver advanced, real-time monitoring solutions to meet your needs.
                     </div>
 
                     {/* <button className='button'>
@@ -26,9 +36,7 @@ const Main = () => {
                 </div>
             </div>
 
-            <div className='mainImg'>
-                <img src={img} />
-            </div>
+
         </div>
     )
 }
