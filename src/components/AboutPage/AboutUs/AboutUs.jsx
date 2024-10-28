973bd5d8bb34375316281216e2d9a47707caa0e5
Mansha <manshapreet02@gmail.com>
2024-10-28 15:51:01 +0530

    why choose us done

--- src/components/AboutPage/AboutUs/AboutUs.jsx
+++ src/components/AboutPage/AboutUs/AboutUs.jsx
@@ -8,6 +8,16 @@ const AboutUs = () => {
             <div className='aboutUsBox'>
                 <div className='aboutUsContent'>
                     <div className='aboutHead'>
+                        Vision & Mission
+                    </div>
+                    <div>
+                        To be a global leader in innovative and reliable surveillance solutions, empowering businesses, homes, and communities with state-of-the-art technology to create safer environments for everyone.
+                    </div>
+
+                    <div>
+                        To provide high-quality, affordable, and customized CCTV solutions that meet the unique security needs of our clients. We are committed to leveraging the latest technologies and delivering exceptional service to ensure peace of mind, real-time monitoring, and long-term safety for all.
+                    </div>
+                    <div className='aboutHead'>
                         About Us
                     </div>
                     <div>
--- src/components/AboutPage/MissionVision/MissionVision.jsx
+++ src/components/AboutPage/MissionVision/MissionVision.jsx
@@ -1,19 +1,81 @@
 import React from 'react'
 import './MissionVision.css'
+import expertise from '../../../assets/icons/Expertise.png'
+import customised from '../../../assets/icons/Customized Solutions.png'
+import support from '../../../assets/icons/24_7 Support.png'
+import afford from '../../../assets/icons/Affordable Pricing.png'
+import custsatis from '../../../assets/icons/Customer Satisfaction.png'
 
 const MissionVision = () => {
     return (
         <div className='MissionVision'>
             <div className='MV_head'>
-                Vision & Mission
-            </div>
-            <div>
-                To be a global leader in innovative and reliable surveillance solutions, empowering businesses, homes, and communities with state-of-the-art technology to create safer environments for everyone.
+                Why Choose Us?
             </div>
 
-            <div>
-                To provide high-quality, affordable, and customized CCTV solutions that meet the unique security needs of our clients. We are committed to leveraging the latest technologies and delivering exceptional service to ensure peace of mind, real-time monitoring, and long-term safety for all.
+            <div className='WCU_card'>
+                <div className='WCU_img'>
+                    <img src={expertise} />
+                </div>
+                <div className='WCU_content'>
+                    <div className='WCU_head'>Expertise</div>
+                    <div className='WCU_text'>
+                        Our team of security experts has in-depth knowledge of the latest CCTV technologies and best practices.
+                    </div>
+                </div>
             </div>
+
+
+            <div className='WCU_card right'>
+                <div className='WCU_content right'>
+                    <div className='WCU_head'>Customized Solutions</div>
+                    <div className='WCU_text'>
+                        We tailor our CCTV systems to suit the unique security requirements of each client.
+                    </div>
+                </div>
+                <div className='WCU_img'>
+                    <img src={customised} />
+                </div>
+            </div>
+
+            <div className='WCU_card'>
+                <div className='WCU_img'>
+                    <img src={support} />
+                </div>
+                <div className='WCU_content'>
+                    <div className='WCU_head'>24/7 Support</div>
+                    <div className='WCU_text'>
+                        We offer round-the-clock technical support and maintenance services to ensure your system operates flawlessly.
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='WCU_card right'>
+                <div className='WCU_content right'>
+                    <div className='WCU_head'>Affordable Pricing</div>
+                    <div className='WCU_text'>
+                        We provide competitive pricing without compromising on quality or performance.
+                    </div>
+                </div>
+                <div className='WCU_img'>
+                    <img src={afford} />
+                </div>
+            </div>
+
+            <div className='WCU_card'>
+                <div className='WCU_img'>
+                    <img src={custsatis} />
+                </div>
+                <div className='WCU_content'>
+                    <div className='WCU_head'>Customer Satisfaction</div>
+                    <div className='WCU_text'>
+                        Our top priority is to deliver solutions that exceed our customers' expectations and ensure their complete satisfaction
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     )
 }
